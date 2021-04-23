760c300b19e00119b7b85b7d9b4c1dc3c2c842b7
langyo <langyo.china@gmail.com>
2021-04-24 03:04:17 +0800

    📝 配置了 mirai 环境与 TypeScript 脚手架。

---
+++ index.ts
@@ -0,0 +1,24 @@
+import Mirai, { Message, check } from 'mirai-ts';
+
+const qq = 1931838621;
+
+const mirai = new Mirai({
+  host: '127.0.0.1',
+  port: 9233,
+  authKey: '114514aaaaaaaa',
+  enableWebsocket: true,
+});
+
+(async () => {
+  await mirai.link(qq);
+  mirai.on('message', msg => {
+    console.log(msg.plain);
+    if (msg.type === 'GroupMessage') {
+      if (msg.plain === 'test') {
+        console.log('test')
+        msg.reply('test', true);
+      }
+    }
+  });
+  mirai.listen();
+})();
