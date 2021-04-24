e0261592c32629afe4f82a76bbb4af457a8549e7
langyo <langyo.china@gmail.com>
2021-04-24 13:36:31 +0800

    🚀 与配置文件与 MongoDB 数据库建立连接。

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
+// ------
+// Mirai 初始化
+// ------
 import Mirai from 'mirai-ts';
 import { GroupMessage } from 'mirai-ts/dist/types/message-type';
-
-const qq = 1931838621;
 
 const mirai = new Mirai({
   host: '127.0.0.1',
@@ -10,14 +11,103 @@ const mirai = new Mirai({
   enableWebsocket: true,
 });
 
-interface IConfigTools {
+(async () => {
+  const ROBOT_QQ = 1931838621;
+
+  await mirai.link(ROBOT_QQ);
+  mirai.on('message', msg => {
+    if (msg.type === 'GroupMessage') {
+      switch (msg.plain) {
+        case '管理指令提示':
+          msg.reply(` 模块管理器暂不可用`, true);
+          break;
+        case '指令提示':
+          msg.reply(` 指令提示暂不可用`, true);
+          break;
+        default:
+          const args = msg.plain.split(' ');
+          for (const moduleName of Object.keys(triggers)) {
+            if (Object.keys(triggers[moduleName]).indexOf(args[0]) >= 0) {
+              if (Object.keys(triggers[moduleName][args[0]]).indexOf(args[1]) >= 0) {
+                triggers[moduleName][args[0]][args[1]].trigger(
+                  args.slice(2), msg, mirai, stateManagerConstructor(moduleName)
+                );
+                break;
+              } else {
+                triggers[moduleName][args[0]]['*'].trigger(
+                  args.slice(1), msg, mirai, stateManagerConstructor(moduleName)
+                );
+                break;
+              }
+            }
+          }
+          break;
+      }
+    }
+  });
+  mirai.listen();
+})();
+
+// ------
+// 日志对象
+// ------
+export const log = mirai.logger;
+
+// ------
+// 针对全局的信息存贮设施，使用本地文件系统，以 JSON 存储
+// ------
+import { readFileSync, writeFileSync, accessSync } from 'fs';
+import { join } from 'path';
+const GLOBAL_CONFIG_FILE_PATH = join(__dirname, '../config/robotGlobalSettings.json');
+try {
+  accessSync(GLOBAL_CONFIG_FILE_PATH);
+} catch (e) {
+  log.info('新的全局配置文件已建立');
+  writeFileSync(GLOBAL_CONFIG_FILE_PATH, JSON.stringify({}));
+}
+
+let globalState = JSON.parse(readFileSync(GLOBAL_CONFIG_FILE_PATH, 'utf-8'));
+log.info('全局配置文件已准备完毕');
+function getGlobalState(module: string, key: string) {
+  if (
+    typeof globalState[module] !== 'undefined' &&
+    typeof globalState[module][key] !== 'undefined'
+  ) {
+    return globalState[module][key];
+  } else {
+    return undefined;
+  }
+}
+function setGlobalState(module: string, key: string, value: string) {
+  if (typeof globalState[module] === 'undefined') {
+    globalState[module] = {};
+  }
+  globalState[module][key] = value;
+  writeFileSync(GLOBAL_CONFIG_FILE_PATH, JSON.stringify(globalState));
+}
+
+// ------
+// 针对各个用户的信息存贮设施，使用 Mongodb 数据库存储
+// ------
+import { connect, connection as db } from 'mongoose';
+connect('mongodb://localhost/chino-mirai-bot', {
+  useNewUrlParser: true, useUnifiedTopology: true
+});
+db.once('open', () => {
+  log.info('本地数据库已建立连接');
+});
+db.on('error', err => {
+  log.error(err);
+})
+
+interface IStateManager {
   get(qq: number, key: string): string,
   getGlobal(key: string): string,
   set(qq: number, key: string, val: string): void,
   setGlobal(key: string, val: string): void
 }
 
-function configToolsConstructor(moduleName: string): Readonly<IConfigTools> {
+function stateManagerConstructor(moduleName: string): Readonly<IStateManager> {
   return Object.seal({
     get(qq: number, key: string): string {
       return '';
@@ -32,8 +122,11 @@ function configToolsConstructor(moduleName: string): Readonly<IConfigTools> {
   });
 }
 
+// ------
+// 模块管理器
+// ------
 type ITrigger = (
-  args: string[], msg: GroupMessage, ctx: Mirai, config: IConfigTools
+  args: string[], msg: GroupMessage, ctx: Mirai, state: IStateManager
 ) => Promise<void>;
 
 let triggers: {
@@ -78,6 +171,9 @@ export function registerCommand(
   }
 }
 
+// ------
+// 批量载入模块
+// ------
 import './goodMorning';
 import './imageLibrary';
 import './judgement';
@@ -90,38 +186,3 @@ import './twitterMonitor';
 import './qrcodeParser';
 import './counterWithdrawal';
 import './permissionForwarding';
-
-(async () => {
-  await mirai.link(qq);
-  mirai.on('message', msg => {
-    if (msg.type === 'GroupMessage') {
-      switch (msg.plain) {
-        case '管理指令提示':
-          msg.reply(` 模块管理器暂不可用`, true);
-          break;
-        case '指令提示':
-          msg.reply(` 指令提示暂不可用`, true);
-          break;
-        default:
-          const args = msg.plain.split(' ');
-          for (const moduleName of Object.keys(triggers)) {
-            if (Object.keys(triggers[moduleName]).indexOf(args[0]) >= 0) {
-              if (Object.keys(triggers[moduleName][args[0]]).indexOf(args[1]) >= 0) {
-                triggers[moduleName][args[0]][args[1]].trigger(
-                  args.slice(2), msg, mirai, configToolsConstructor(moduleName)
-                );
-                break;
-              } else {
-                triggers[moduleName][args[0]]['*'].trigger(
-                  args.slice(1), msg, mirai, configToolsConstructor(moduleName)
-                );
-                break;
-              }
-            }
-          }
-          break;
-      }
-    }
-  });
-  mirai.listen();
-})();
